6b7c3f32bedef59eb350f38fece675e48115ac48
bhappyholistico <91427614+bhappyholistico@users.noreply.github.com>
2023-01-15 14:16:52 +0000

    JAN23.2

---
+++ js/popup.js
@@ -0,0 +1,458 @@
+let buttonAbraco = document.getElementById('abraco');
+
+buttonAbraco.addEventListener('click', function(evt){
+    document.getElementById('bgPopup').classList.add('show');
+    document.getElementById('popupAbraco').classList.add('show');
+});
+
+let buttonCloseAbraco = document.getElementById('buttonCloseAbraco');
+
+buttonCloseAbraco.addEventListener('click', function(evt){
+    document.getElementById('bgPopup').classList.remove('show');
+    document.getElementById('popupAbraco').classList.remove('show');
+
+});
+
+//
+
+let buttonAconselhamento = document.getElementById('aconselhamento');
+
+buttonAconselhamento.addEventListener('click', function(evt){
+    document.getElementById('bgPopup').classList.add('show');
+    document.getElementById('popupAconselhamento').classList.add('show');
+});
+
+let buttonCloseAconselhamento = document.getElementById('buttonCloseAconselhamento');
+
+buttonCloseAconselhamento.addEventListener('click', function(evt){
+    document.getElementById('bgPopup').classList.remove('show');
+    document.getElementById('popupAconselhamento').classList.remove('show');
+
+});
+
+//
+
+let buttonAcm = document.getElementById('acm');
+
+buttonAcm.addEventListener('click', function(evt){
+    document.getElementById('bgPopup').classList.add('show');
+    document.getElementById('popupAcm').classList.add('show');
+});
+
+let buttonCloseAcm = document.getElementById('buttonCloseAcm');
+
+buttonCloseAcm.addEventListener('click', function(evt){
+    document.getElementById('bgPopup').classList.remove('show');
+    document.getElementById('popupAcm').classList.remove('show');
+
+});
+
+//
+
+let buttonBarras = document.getElementById('barras');
+
+buttonBarras.addEventListener('click', function(evt){
+    document.getElementById('bgPopup').classList.add('show');
+    document.getElementById('popupBarras').classList.add('show');
+});
+
+let buttonCloseBarras = document.getElementById('buttonCloseBarras');
+
+buttonCloseBarras.addEventListener('click', function(evt){
+    document.getElementById('bgPopup').classList.remove('show');
+    document.getElementById('popupBarras').classList.remove('show');
+
+});
+
+//
+
+let buttonBio = document.getElementById('bio');
+
+buttonBio.addEventListener('click', function(evt){
+    document.getElementById('bgPopup').classList.add('show');
+    document.getElementById('popupBio').classList.add('show');
+});
+
+let buttonCloseBio = document.getElementById('buttonCloseBio');
+
+buttonCloseBio.addEventListener('click', function(evt){
+    document.getElementById('bgPopup').classList.remove('show');
+    document.getElementById('popupBio').classList.remove('show');
+
+});
+
+//
+
+let buttonConstelacao = document.getElementById('constelacao');
+
+buttonConstelacao.addEventListener('click', function(evt){
+    document.getElementById('bgPopup').classList.add('show');
+    document.getElementById('popupConstelacao').classList.add('show');
+});
+
+let buttonCloseConstelacao = document.getElementById('buttonCloseConstelacao');
+
+buttonCloseConstelacao.addEventListener('click', function(evt){
+    document.getElementById('bgPopup').classList.remove('show');
+    document.getElementById('popupConstelacao').classList.remove('show');
+
+});
+
+//
+
+let buttonLeituraIntuitiva = document.getElementById('consulta-intuitiva');
+
+buttonLeituraIntuitiva.addEventListener('click', function(evt){
+    document.getElementById('bgPopup').classList.add('show');
+    document.getElementById('popupLeituraIntuitiva').classList.add('show');
+});
+
+let buttonCloseLeituraIntuitiva = document.getElementById('buttonCloseLeituraIntuitiva');
+
+buttonCloseLeituraIntuitiva.addEventListener('click', function(evt){
+    document.getElementById('bgPopup').classList.remove('show');
+    document.getElementById('popupLeituraIntuitiva').classList.remove('show');
+
+});
+
+//
+
+let buttonProposito = document.getElementById('consulta-proposito');
+
+buttonProposito.addEventListener('click', function(evt){
+    document.getElementById('bgPopup').classList.add('show');
+    document.getElementById('popupProposito').classList.add('show');
+});
+
+let buttonCloseProposito = document.getElementById('buttonCloseProposito');
+
+buttonCloseProposito.addEventListener('click', function(evt){
+    document.getElementById('bgPopup').classList.remove('show');
+    document.getElementById('popupProposito').classList.remove('show');
+
+});
+
+//
+
+let buttonConsulta = document.getElementById('consulta');
+
+buttonConsulta.addEventListener('click', function(evt){
+    document.getElementById('bgPopup').classList.add('show');
+    document.getElementById('popupConsulta').classList.add('show');
+});
+
+let buttonCloseConsulta = document.getElementById('buttonCloseConsulta');
+
+buttonCloseConsulta.addEventListener('click', function(evt){
+    document.getElementById('bgPopup').classList.remove('show');
+    document.getElementById('popupConsulta').classList.remove('show');
+
+});
+
+//
+
+let buttonHipnose = document.getElementById('hipnose');
+
+buttonHipnose.addEventListener('click', function(evt){
+    document.getElementById('bgPopup').classList.add('show');
+    document.getElementById('popupHipnose').classList.add('show');
+});
+
+let buttonCloseHipnose = document.getElementById('buttonCloseHipnose');
+
+buttonCloseHipnose.addEventListener('click', function(evt){
+    document.getElementById('bgPopup').classList.remove('show');
+    document.getElementById('popupHipnose').classList.remove('show');
+
+});
+
+//
+
+let buttonAura = document.getElementById('aura');
+
+buttonAura.addEventListener('click', function(evt){
+    document.getElementById('bgPopup').classList.add('show');
+    document.getElementById('popupAura').classList.add('show');
+});
+
+let buttonCloseAura = document.getElementById('buttonCloseAura');
+
+buttonCloseAura.addEventListener('click', function(evt){
+    document.getElementById('bgPopup').classList.remove('show');
+    document.getElementById('popupAura').classList.remove('show');
+
+});
+
+//
+
+let buttonLimpezaEnergetica = document.getElementById('limpeza-energetica');
+
+buttonLimpezaEnergetica.addEventListener('click', function(evt){
+    document.getElementById('bgPopup').classList.add('show');
+    document.getElementById('popupLimpezaEnergetica').classList.add('show');
+});
+
+let buttonCloseLimpezaEnergetica = document.getElementById('buttonCloseLimpezaEnergetica');
+
+buttonCloseLimpezaEnergetica.addEventListener('click', function(evt){
+    document.getElementById('bgPopup').classList.remove('show');
+    document.getElementById('popupLimpezaEnergetica').classList.remove('show');
+
+});
+
+//
+
+let buttonLinguagem = document.getElementById('linguagem-luz');
+
+buttonLinguagem.addEventListener('click', function(evt){
+    document.getElementById('bgPopup').classList.add('show');
+    document.getElementById('popupLinguagem').classList.add('show');
+});
+
+let buttonCloseLinguagem = document.getElementById('buttonCloseLinguagem');
+
+buttonCloseLinguagem.addEventListener('click', function(evt){
+    document.getElementById('bgPopup').classList.remove('show');
+    document.getElementById('popupLinguagem').classList.remove('show');
+
+});
+
+//
+
+let buttonMapaAstral = document.getElementById('mapa-astral');
+
+buttonMapaAstral.addEventListener('click', function(evt){
+    document.getElementById('bgPopup').classList.add('show');
+    document.getElementById('popupMapaAstral').classList.add('show');
+});
+
+let buttonCloseMapaAstral = document.getElementById('buttonCloseMapaAstral');
+
+buttonCloseMapaAstral.addEventListener('click', function(evt){
+    document.getElementById('bgPopup').classList.remove('show');
+    document.getElementById('popupMapaAstral').classList.remove('show');
+
+});
+
+//
+
+let buttonRelaxamento = document.getElementById('relaxamento');
+
+buttonRelaxamento.addEventListener('click', function(evt){
+    document.getElementById('bgPopup').classList.add('show');
+    document.getElementById('popupRelaxamento').classList.add('show');
+});
+
+let buttonCloseRelaxamento = document.getElementById('buttonCloseRelaxamento');
+
+buttonCloseRelaxamento.addEventListener('click', function(evt){
+    document.getElementById('bgPopup').classList.remove('show');
+    document.getElementById('popupRelaxamento').classList.remove('show');
+
+});
+
+//
+
+let buttonMentoria = document.getElementById('mentoria');
+
+buttonMentoria.addEventListener('click', function(evt){
+    document.getElementById('bgPopup').classList.add('show');
+    document.getElementById('popupMentoria').classList.add('show');
+});
+
+let buttonCloseMentoria = document.getElementById('buttonCloseMentoria');
+
+buttonCloseMentoria.addEventListener('click', function(evt){
+    document.getElementById('bgPopup').classList.remove('show');
+    document.getElementById('popupMentoria').classList.remove('show');
+
+});
+
+//
+
+let buttonNumerologia = document.getElementById('numerologia');
+
+buttonNumerologia.addEventListener('click', function(evt){
+    document.getElementById('bgPopup').classList.add('show');
+    document.getElementById('popupNumerologia').classList.add('show');
+});
+
+let buttonCloseNumerologia = document.getElementById('buttonCloseNumerologia');
+
+buttonCloseNumerologia.addEventListener('click', function(evt){
+    document.getElementById('bgPopup').classList.remove('show');
+    document.getElementById('popupNumerologia').classList.remove('show');
+
+});
+
+//
+
+let buttonReflexologia = document.getElementById('reflexologia');
+
+buttonReflexologia.addEventListener('click', function(evt){
+    document.getElementById('bgPopup').classList.add('show');
+    document.getElementById('popupReflexologia').classList.add('show');
+});
+
+let buttonCloseReflexologia = document.getElementById('buttonCloseReflexologia');
+
+buttonCloseReflexologia.addEventListener('click', function(evt){
+    document.getElementById('bgPopup').classList.remove('show');
+    document.getElementById('popupReflexologia').classList.remove('show');
+
+});
+
+//
+
+let buttonReiki = document.getElementById('reiki');
+
+buttonReiki.addEventListener('click', function(evt){
+    document.getElementById('bgPopup').classList.add('show');
+    document.getElementById('popupReiki').classList.add('show');
+});
+
+let buttonCloseReiki = document.getElementById('buttonCloseReiki');
+
+buttonCloseReiki.addEventListener('click', function(evt){
+    document.getElementById('bgPopup').classList.remove('show');
+    document.getElementById('popupReiki').classList.remove('show');
+
+});
+
+//
+
+let buttonAkashico = document.getElementById('akashico');
+
+buttonAkashico.addEventListener('click', function(evt){
+    document.getElementById('bgPopup').classList.add('show');
+    document.getElementById('popupAkashico').classList.add('show');
+});
+
+let buttonCloseAkashico = document.getElementById('buttonCloseAkashico');
+
+buttonCloseAkashico.addEventListener('click', function(evt){
+    document.getElementById('bgPopup').classList.remove('show');
+    document.getElementById('popupAkashico').classList.remove('show');
+
+});
+
+//
+
+let buttonAuricular = document.getElementById('auricular');
+
+buttonAuricular.addEventListener('click', function(evt){
+    document.getElementById('bgPopup').classList.add('show');
+    document.getElementById('popupAuricular').classList.add('show');
+});
+
+let buttonCloseAuricular = document.getElementById('buttonCloseAuricular');
+
+buttonCloseAuricular.addEventListener('click', function(evt){
+    document.getElementById('bgPopup').classList.remove('show');
+    document.getElementById('popupAuricular').classList.remove('show');
+
+});
+
+//
+
+let buttonTarot = document.getElementById('tarot');
+
+buttonTarot.addEventListener('click', function(evt){
+    document.getElementById('bgPopup').classList.add('show');
+    document.getElementById('popupTarot').classList.add('show');
+});
+
+let buttonCloseTarot = document.getElementById('buttonCloseTarot');
+
+buttonCloseTarot.addEventListener('click', function(evt){
+    document.getElementById('bgPopup').classList.remove('show');
+    document.getElementById('popupTarot').classList.remove('show');
+
+});
+
+//
+
+let buttonCirculo = document.getElementById('circulo');
+
+buttonCirculo.addEventListener('click', function(evt){
+    document.getElementById('bgPopup').classList.add('show');
+    document.getElementById('popupCirculo').classList.add('show');
+});
+
+let buttonCloseCirculo = document.getElementById('buttonCloseCirculo');
+
+buttonCloseCirculo.addEventListener('click', function(evt){
+    document.getElementById('bgPopup').classList.remove('show');
+    document.getElementById('popupCirculo').classList.remove('show');
+
+});
+
+//
+
+let buttonConcerto = document.getElementById('concerto');
+
+buttonConcerto.addEventListener('click', function(evt){
+    document.getElementById('bgPopup').classList.add('show');
+    document.getElementById('popupConcerto').classList.add('show');
+});
+
+let buttonCloseConcerto = document.getElementById('buttonCloseConcerto');
+
+buttonCloseConcerto.addEventListener('click', function(evt){
+    document.getElementById('bgPopup').classList.remove('show');
+    document.getElementById('popupConcerto').classList.remove('show');
+
+});
+
+//
+
+let buttonConstelacoes = document.getElementById('constelacoes');
+
+buttonConstelacoes.addEventListener('click', function(evt){
+    document.getElementById('bgPopup').classList.add('show');
+    document.getElementById('popupConstelacoes').classList.add('show');
+});
+
+let buttonCloseConstelacoes = document.getElementById('buttonCloseConstelacoes');
+
+buttonCloseConstelacoes.addEventListener('click', function(evt){
+    document.getElementById('bgPopup').classList.remove('show');
+    document.getElementById('popupConstelacoes').classList.remove('show');
+
+});
+
+//
+
+let buttonSintonizacao = document.getElementById('sintonizacao');
+
+buttonSintonizacao.addEventListener('click', function(evt){
+    document.getElementById('bgPopup').classList.add('show');
+    document.getElementById('popupSintonizacao').classList.add('show');
+});
+
+let buttonCloseSintonizacao = document.getElementById('buttonCloseSintonizacao');
+
+buttonCloseSintonizacao.addEventListener('click', function(evt){
+    document.getElementById('bgPopup').classList.remove('show');
+    document.getElementById('popupSintonizacao').classList.remove('show');
+
+});
+
+//
+
+let buttonVapor = document.getElementById('vapor');
+
+buttonVapor.addEventListener('click', function(evt){
+    document.getElementById('bgPopup').classList.add('show');
+    document.getElementById('popupVapor').classList.add('show');
+});
+
+let buttonCloseVapor = document.getElementById('buttonCloseVapor');
+
+buttonCloseVapor.addEventListener('click', function(evt){
+    document.getElementById('bgPopup').classList.remove('show');
+    document.getElementById('popupVapor').classList.remove('show');
+
+});
+
+//
